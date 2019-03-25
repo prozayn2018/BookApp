e58e2f3f6e8b29943d3517f32c74551ec4ee5863
Zayneel <zayneel@berkeley.edu>
2019-03-25 16:01:36 -0700

    final version

--- booklibrary.js
+++ booklibrary.js
@@ -1,14 +1,40 @@
-let p = document.querySelector('p');
-x = p.addEventListener('mouseover', function(){
-  p.style.color = 'red'
+
+$('p').mouseout(function(){
+  $(this).css('color', 'orange')
 })
 
 $(document).ready(function(){
 
-  $('p').mouseout(function(){
-    $(this).css('color', 'orange')
-  })
+// when addBook is clicked
+var gridItemCount = 1;
 
+
+    const addButtClicked = $('#addbut').click(function(){
+
+      //logic for adding a new postit stamp to view;
+      function gridItem(){
+        var cloneGrid = $('#1').clone();
+        gridItemCount++;
+        //gives each gridItem a unique id
+        cloneGrid.attr("id", gridItemCount);
+        console.log(cloneGrid)
+        //gives each gridItem a unique id for "x"; delete button
+        cloneGrid.children("i").attr("id", 'x'+gridItemCount);
+        // deletes value on clone post if post it 1 has values inside
+        var parentOne = cloneGrid.children("p");
+        var childOne = parentOne.children("textarea").val("");
+        var updateContainer = cloneGrid.appendTo('.grid-container');
+        //gives each gridItem a unique id for "x"; delete button
+
+      };
+      gridItem()
+    });
+
+    //logic for removing posititstamp;
+    function deletePostit(){
+    const deleteButtClicked = $(document).on('click', 'i', function(){
+      $(this).parent().remove();
+    });
+  };
+ deletePostit();
 });
-
-//everything is linked between JS, html, and CSS.
